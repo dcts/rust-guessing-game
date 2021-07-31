4221cac9172fbf6ee239424c23c8accf953c8f86
dcts <thomas@liist.com>
2021-07-31 03:54:17 +0200

    :sparkle: working guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,30 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
     println!("=== Guess the number game ===");
-    println!("please insert your guess: ");
+    // create secret number
+    let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    let mut guess = String::new();
-    const Y: char = '2';
+    loop {
+        println!("please insert your guess: ");
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read input");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read input");
 
-    println!("you guessed {}. \ny is {}", guess, Y);
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Equal => {
+                println!("YOU WON");
+                break;
+            }
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+        }
+    }
 }
 
